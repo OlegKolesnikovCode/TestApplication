7c9c79f954ed478dcc46ad4876ca7327832d438c
Oleg Kolesnikov <olegkolesnikov2024@gmail.com>
2025-04-13 16:10:58 -0700

    Created a Powershell Function to automate the add, commit and push Git functionality with one command

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -5,11 +5,21 @@ import './styles.css';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+
+const router = createBrowserRouter([
+
+{
+    path: '/',
+    element: <App/>,
+}
+
+]);
+
 //Current and recommended way to initialize and render a React application, introduced in React 18 (released in March 2022).
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
 <React.StrictMode>
-<App />
+<RouterProvider router={router}/>
 </React.StrictMode>
 );
 
