eb1eb4c4dd132341b73275fb6b49effdd953a90d
Oleg Kolesnikov <olegkolesnikov2024@gmail.com>
2025-04-13 15:30:37 -0700

    npm install react-router-dom

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles.css';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+//Current and recommended way to initialize and render a React application, introduced in React 18 (released in March 2022).
 const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(<App />);
+root.render(
+<React.StrictMode>
+<App />
+</React.StrictMode>
+);
+
